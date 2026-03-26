46134c8233c85e06a8ea705996a8f3663067b326
Kelli Kirk <kelli.kirk@voco.ee>
2026-03-26 10:37:57 +0200

    Create project base files and functionalities

---
+++ client/src/PostList.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import CommentsList from './CommentsList'
+import CommentCreate from './CommentCreate'
+
+const PostList = () => {
+    const [posts, setPosts]  = useState([])   
+ 
+
+useEffect(() => {
+    const fetchPosts = async () => {
+        try {
+            const res = await axios.get('http://localhost:5000/posts')
+            const posts = res.data
+            setPosts(posts)
+        } catch (err) {
+            console.error(err)
+        } 
+    } 
+    fetchPosts()
+},[])
+
+console.log(posts)
+
+const postsForRender = posts.map(post => {
+    return (
+        <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
+            <div className="card-body">
+                <h3>{post.title}</h3>
+                <CommentsList postid={post.id} />
+                <CommentCreate postid={post.id} /> 
+            </div>
+        </div>
+    )
+})
+
+return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+       {postsForRender} 
+    </div>
+)
+
+}
+
+
+export default PostList
